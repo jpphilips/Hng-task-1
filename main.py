91b6605eb93e3c5ff705bc4a194355db10a18e2c
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-10-29 03:51:22 +0100

    updated for CORS

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
-    allow_headers=['*'])
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 
 @app.get("/")
