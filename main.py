8fd30385eea00fa57996b1b61e4b11b53c282a42
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-10-29 03:33:13 +0100

    updated for CORS

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.middleware.core import CORSMiddleware
 
 app = FastAPI(
     title="College API",
     description="An api endoint that returns a json response for Hng internship",
-    version="1",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_headers=['*'])
 
 
 @app.get("/")
